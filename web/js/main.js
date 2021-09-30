c7f19597975d23461680ac8865fe182aed0d03f1
Jože Strožer <jozistrozer13@gmail.com>
2021-09-30 22:13:53 +0200

    Dodan layout za spletno stran

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,17 @@
-function Klik() {
-    eel.funFunkcija();
-}
+$(document).ready(function(){
+  $("#lnkDodaj").click(function(){
+    location.href = "index.html";
+  });
+
+  $("#lnkDnevnik").click(function(){
+    location.href = "dnevnik.html";
+  });
+
+  $("#lnkPDF").click(function(){
+    location.href = "generatePdf.html";
+  });
+
+  $("#lnkMe").click(function(){
+
+  });
+});
